8cec6cb5467f63bcbc21638e4e6041020c5c1b4a
Samer taha <alrazy1@gmail.com>
2022-07-03 17:20:39 +0300

    added samer

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.json({ message: 'hellow backend' });
+  res.json({ message: 'hellow samer from backend' });
 });
 
 app.listen(3000);

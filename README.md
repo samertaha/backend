bccda26f85461c1a7152d27c57811328ca3b41ae
Samer taha <alrazy1@gmail.com>
2022-07-07 17:55:19 +0300

    wow

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # hackaton-back
 
 # hackaton-backen
+
+# 022/07/07 13:27:54 [error] 3586#3586: \*44 connect() failed (111: Unknown error) while connecting to upstream, clien>2022/
--- app.js
+++ app.js
@@ -23,4 +23,5 @@ app.use('/api/matches', require('./routes/userRoutes'));
 app.use(errorHandler);
 scraper();
 
-app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+//app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+app.listen(8000);
